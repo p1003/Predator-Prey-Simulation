2e7091dd17389d5136d4eed056aa41aca8042d0c
p1003 <56313631+p1003@users.noreply.github.com>
2022-05-03 11:39:10 +0200

    init genome

--- config.py
+++ config.py
@@ -12,4 +12,8 @@ class Config:
         self.plant_regeneration_ratio = 0.2
         self.max_plant_supply = 15
         self.minimal_reproduction_energy = 30
+
+        self.simulate_genomes = True
+        self.viewrange_range = (0, 5)
+        self.energy_consumption_ratio_range = (0.5, 1.5)
         
