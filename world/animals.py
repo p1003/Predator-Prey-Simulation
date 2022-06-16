4d37a8e00f967047875a736b1fbd25f5757ebd95
Łukasz Pitrus <56313631+p1003@users.noreply.github.com>
2022-06-16 17:53:14 +0200

    Toroid map and decisions (#10)
    * toroid map
    
    * decision making
    
    * direction weights changes
    
    * genes
    
    * conversation resolving
    
    * errors repaired

--- world/animals.py
+++ world/animals.py
@@ -7,6 +7,8 @@ from config import Config
 from world.enumerators import Species, Directions
 from world.genome import Genome
 
+import numpy as np
+
 if TYPE_CHECKING:
     from world import Map
     from world.map import MapTile
@@ -14,13 +16,14 @@ if TYPE_CHECKING:
 
 
 class Animal:
-    """
-    Tracks the animal's position, energy, species (rabbit/fox) and state (live/dead).
-    """
     n_prey = 0
     n_predator = 0
 
-    def __init__(self, x: int, y: int, init_energy: int, species: Species, id: int, map: Map, config: Config, genome: Genome = Genome()):
+    def __init__(self, x: int, y: int, init_energy: int, species: Species, id: int, map: Map, config: Config, genome: Genome = None):
+        if genome is None:
+            self.genome = Genome(config=config)
+        else:
+            self.genome = genome
         self.x = x
         self.y = y
         self.energy = init_energy
@@ -29,7 +32,6 @@ class Animal:
         self.isDead: bool = False
         self.map = map
         self.config = config
-        self.genome = genome
         self.energy_consumption = self.genome.calculate_energy_consumption()
 
         if self.species == Species.PREY:
@@ -40,8 +42,6 @@ class Animal:
             raise ValueError(f'{self.species} is a wrong species')
 
     def interact(self, other: Animal):
-        """
-        """
         if self.species == other.species:
             if self.energy > self.config.minimal_reproduction_energy and other.energy > self.config.minimal_reproduction_energy:
                 new_self_energy = self.energy // 3 * 2
@@ -50,15 +50,23 @@ class Animal:
                 self.energy = new_self_energy
                 other.energy = new_other_energy
                 x, y = self.get_position()
-                self.map.add_child(x=x, y=y, init_energy=child_energy, species=self.species)
+                self.map.add_child(
+                    x=x, y=y, init_energy=child_energy, species=self.species, 
+                    genome=Genome.combined_genome(
+                        first=self.genome,
+                        second=other.genome,
+                        config=self.config
+                    ))
 
         elif self.species == Species.PREY and other.species == Species.PREDATOR:
-            self.die()
-            other.energy += self.energy
+            if not other.check_if_energy_over_max():
+                self.die()
+                other.energy += int(self.energy*self.config.food_efficiency_ratio)
 
         elif self.species == Species.PREDATOR and other.species == Species.PREY:
-            other.die()
-            self.energy += other.energy
+            if not self.check_if_energy_over_max():
+                other.die()
+                self.energy += int(other.energy*self.config.food_efficiency_ratio)
 
     def die(self):
         if not self.isDead:
@@ -69,42 +77,103 @@ class Animal:
                 Animal.n_predator -= 1
 
     def move(self, direction, gridxsize, gridysize):
-        """
-        Move a step on the grid. Each step consumes 1 energy; if no energy left, die.
-        If hitting the bounds of the grid, "bounde back", step to the opposite direction insetad.
-
-        Arguments:
-            direction {int} -- direction to move: UP: 0, DOWN: 1, LEFT: 2, RIGHT: 3, STAY: 4
-        """
         energy_int = int(self.energy_consumption)
-        if random() > self.energy_consumption % 1:
+        if random() > self.energy_consumption % 1 and self.energy_consumption != 1.:
             energy_int += 1
         self.energy = max(0, self.energy - energy_int)
 
-        # TODO: toroid map movement
         if direction == Directions.LEFT:
-            self.x -= 1 if self.x > 0 else -1
+            self.x = (self.x + gridxsize - 1) % gridxsize
         if direction == Directions.RIGHT:
-            self.x += 1 if self.x < gridxsize - 1 else -1
+            self.x = (self.x + gridxsize + 1) % gridxsize
         if direction == Directions.UP:
-            self.y += 1 if self.y < gridysize - 1 else -1
+            self.y = (self.y + gridysize - 1) % gridysize
         if direction == Directions.DOWN:
-            self.y -= 1 if self.y > 0 else -1
+            self.y = (self.y + gridysize + 1) % gridysize
         if direction == Directions.STAY:
             pass
 
         if self.energy <= 0:
             self.die()  # R.I.P.
 
-    def choose_direction(self):
-        if self.config.simulate_genomes:
-            # TODO: different behavior for prey and predator
-            x, y = self.get_position()
-            neighbourhood: list[list[MapTile]] = self.map.get_submap(x=x, y=y, radius=int(self.genome.viewrange))
-            # TODO: detection of other animals and food
+    def _calc_tile_choice_value(self, current_tile: MapTile) -> float:
+        result = 0.0
+        if not current_tile.is_empty():
+            prey_n, predator_n = current_tile.get_animal_counts()
+            if self.species == Species.PREY:
+                result -= predator_n * self.genome.fear_of_predator_ratio
+                result += prey_n / self.genome.eating_over_mating_ratio
+                result += current_tile.get_n_plants() * self.genome.eating_over_mating_ratio
+            else:
+                result += predator_n / self.genome.eating_over_mating_ratio
+                result += prey_n * self.genome.eating_over_mating_ratio
+        return result
+
+    def choose_direction(self) -> Directions:
+        if not self.config.simulate_genomes:
             return choice(list(Directions))
-        else:
-            return choice(list(Directions))
+        x, y = self.get_position()
+        current_viewrange = int(self.genome.viewrange)
+        if random() > self.genome.viewrange % 1 and self.genome.viewrange != 1.:
+            current_viewrange += 1
+
+        neighbourhood: list[list[MapTile]] = self.map.get_submap(x=x, y=y, radius=current_viewrange)
+
+        n_size = current_viewrange*2 + 1
+
+        directions_weights = [1., 1., 1., 1., 1.] # []
+        for i in range(n_size):
+            for j in range(n_size):
+                tile_choice_value = self._calc_tile_choice_value(current_tile=neighbourhood[i][j])
+                x_dist = abs(i-current_viewrange)
+                y_dist = abs(j-current_viewrange)
+                if x_dist == 0 and y_dist == 0:
+                    directions_weights[4] = tile_choice_value
+                    
+                else:
+                    x_direction: Directions
+                    y_direction: Directions
+                    x_weight = float(x_dist)/(x_dist + y_dist)
+                    y_weight = float(y_dist)/(x_dist + y_dist)
+                    distance_weight = 1./(pow(x_dist, 2) + pow(y_dist, 2))
+
+                    if i > current_viewrange:
+                        x_direction = Directions.RIGHT
+                    elif i < current_viewrange:
+                        x_direction = Directions.LEFT
+                    else:
+                        x_direction = Directions.STAY
+
+                    if j > current_viewrange:
+                        y_direction = Directions.DOWN
+                    elif j < current_viewrange:
+                        y_direction = Directions.UP
+                    else:
+                        y_direction = Directions.STAY
+                    
+                    directions_weights[x_direction.value] += tile_choice_value * x_weight * distance_weight
+                    directions_weights[y_direction.value] += tile_choice_value * y_weight * distance_weight
+
+        # below lines handles negative values
+        for i in range(4):
+            if directions_weights[i] < 0.:
+                if i%2 == 0:
+                    directions_weights[i+1] -= directions_weights[i]
+                    directions_weights[i] = 0.
+                else:
+                    directions_weights[i-1] -= directions_weights[i]
+                    directions_weights[i] = 0.
+        
+        if directions_weights[4] < 0.:
+            for i in range(4):
+                directions_weights[i] -= directions_weights[4]/4
+            directions_weights[4] = 0.
+        directions_weights = np.array(directions_weights)
+        directions_weights /= directions_weights.sum()
+        return Directions(np.random.choice(a=np.array([0, 1, 2, 3, 4]), size=1, p=directions_weights))
+
+    def check_if_energy_over_max(self):
+        return self.energy > int(self.genome.get_genes()[2])
 
     def get_position(self):
         return self.x, self.y
--- world/genome.py
+++ world/genome.py
@@ -4,45 +4,77 @@ from random import uniform
 
 from config import Config
 
-N_GENES = 2
 GENE_NAMES = [
     'View range',
-    'Energy consumption ratio'
+    'Energy consumption ratio',
+    'Max animal energy',
+    'Fear of predators',
+    'Eating over mating ratio'
 ]
+N_GENES = len(GENE_NAMES)
 
-# TODO: add cost (energy)
 class Genome:
     """"""
 
-    def __init__(self, viewrange: float = 1., energy_consumption_ratio: float = 1.):
-        self.viewrange = viewrange
-        self.energy_consumption_ratio = energy_consumption_ratio
+    def __init__(self,
+                 viewrange: float = 1.,
+                 energy_consumption_ratio: float = 1.,
+                 max_animal_energy: int = 150,
+                 fear_of_predator_ratio: float = 1.,
+                 eating_over_mating_ratio: float = 1.,
+                 config: Config = None
+                 ):
+        if config is not None:
+            self.viewrange = (config.viewrange_range[0] + config.viewrange_range[1]/2)
+            self.energy_consumption_ratio = (config.energy_consumption_ratio_range[0] + config.energy_consumption_ratio_range[1]/2)
+            self.max_animal_energy = (config.max_animal_energy[0] + config.max_animal_energy[1]/2)
+            self.fear_of_predator_ratio = (config.fear_of_predator_ratio[0] + config.fear_of_predator_ratio[1]/2)
+            self.eating_over_mating_ratio = (config.eating_over_mating_ratio[0] + config.eating_over_mating_ratio[1]/2)
+        else:
+            self.viewrange = viewrange
+            self.energy_consumption_ratio = energy_consumption_ratio
+            self.max_animal_energy = max_animal_energy
+            self.fear_of_predator_ratio = fear_of_predator_ratio
+            self.eating_over_mating_ratio = eating_over_mating_ratio
     
     def calculate_energy_consumption(self) -> float:
-        # TODO: check if this works
         return (self.viewrange) * self.energy_consumption_ratio
 
     def get_genes(self) -> list[float]:
         return [
             self.viewrange,
-            self.energy_consumption_ratio
+            self.energy_consumption_ratio,
+            self.max_animal_energy,
+            self.fear_of_predator_ratio,
+            self.eating_over_mating_ratio
         ]
     
     @staticmethod
-    def mutate_gene(gene: float , config: Config) -> float:
-        return gene + round(uniform(-config.mutation_ratio, config.mutation_ratio), 3)
+    def _mutate_gene(gene: float , config: Config, is_additive: bool) -> float:
+        if is_additive:
+            return max(gene + round(uniform(-config.mutation_ratio, config.mutation_ratio), 3), 0.)
+        return gene * (1. + round(uniform(-config.mutation_ratio, config.mutation_ratio), 3))
     
     @staticmethod
     def combined_genome(first: Genome, second: Genome, config: Config) -> Genome:
+        if not config.simulate_genomes:
+            return first.get_genes()
         first_genes = first.get_genes()
         second_genes = second.get_genes()
 
+        # below value is ugly cheat: genomes below 3rd index mutate additively, equal or higher multiplicatively
+        arbitral_var= 3
+
         new_genes = []
         for i in range(len(first_genes)):
-            new_genes.append(Genome.mutate_gene(gene=(first_genes[i] + second_genes[i])/2, config=config))
-        viewrange_min, viewrange_max = config.viewrange_range
-        energy_consumption_ratio_min, energy_consumption_ratio_max = config.energy_consumption_ratio_range
+            new_genes.append(Genome._mutate_gene(gene=(first_genes[i] + second_genes[i])/2, config=config, is_additive=i<arbitral_var))
+        
+        gene_ranges = config.get_gene_ranges()
+
         return Genome(
-            viewrange=max(viewrange_min, min(viewrange_max, new_genes[0])),
-            energy_consumption_ratio=max(energy_consumption_ratio_min, min(energy_consumption_ratio_max, new_genes[1]))
+            viewrange=max(gene_ranges[0][0], min(gene_ranges[0][1], new_genes[0])),
+            energy_consumption_ratio=max(gene_ranges[1][0], min(gene_ranges[1][1], new_genes[1])),
+            max_animal_energy=max(gene_ranges[2][0], min(gene_ranges[2][1], new_genes[2])),
+            fear_of_predator_ratio=max(gene_ranges[3][0], min(gene_ranges[3][1], new_genes[3])),
+            eating_over_mating_ratio=max(gene_ranges[4][0], min(gene_ranges[4][1], new_genes[4])),
         )
