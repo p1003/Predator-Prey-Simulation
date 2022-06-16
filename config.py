1633e3dd3b2bc61073cdf6881c929c884a4098b9
Mateusz Kocot <56218578+MatiXOfficial@users.noreply.github.com>
2022-06-16 18:00:17 +0200

    Revert "Added the rest of the genes to the config frame"

--- config.py
+++ config.py
@@ -14,24 +14,22 @@ class Config:
         self.minimal_reproduction_energy = 30
 
         self.simulate_genomes = True
-        self.viewrange_range = (0, 5)
+        self.viewrange_range = (0., 5.)
         self.energy_consumption_ratio_range = (0.5, 1.5)
-        self.max_animal_energy_range = (100, 200)
-        self.fear_of_predator_ratio_range = (0.5, 1.5)
-        self.eating_over_mating_ratio_range = (0.5, 1.5)
+        self.max_animal_energy = (100, 200)
+        self.fear_of_predator_ratio = (0.5, 1.5)
+        self.eating_over_mating_ratio = (0.5, 1.5)
 
         self.food_efficiency_ratio = 0.5
 
         self.mutation_ratio = 0.05
-
+    
     def get_gene_ranges(self):
-        return [self.viewrange_range, self.energy_consumption_ratio_range, self.max_animal_energy_range,
-                self.fear_of_predator_ratio_range, self.eating_over_mating_ratio_range]
+        return [
+            self.viewrange_range,
+            self.energy_consumption_ratio_range,
+            self.max_animal_energy,
+            self.fear_of_predator_ratio,
+            self.eating_over_mating_ratio
+        ]
 
-    def set_gene_ranges(self, vierwange_range, energy_consumption_ratio_range, max_animal_energy_range,
-                        fear_of_predator_ratio_range, eating_over_mating_ratio_range):
-        self.viewrange_range = vierwange_range
-        self.energy_consumption_ratio_range = energy_consumption_ratio_range
-        self.max_animal_energy_range = max_animal_energy_range
-        self.fear_of_predator_ratio_range = fear_of_predator_ratio_range
-        self.eating_over_mating_ratio_range = eating_over_mating_ratio_range
